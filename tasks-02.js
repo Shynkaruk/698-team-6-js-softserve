f823e0863d71e2e852ee466f54e3d97573d467ce
VladGraundq <vladdmitrievq@gmail.com>
2022-06-21 10:58:20 +0000

    add half completed tasks

---
+++ tasks-02.js
@@ -0,0 +1,28 @@
+// Solution by Vladislav Kulybaba
+
+
+//1 https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function stringToArray(string) {
+    return string.split(" ");
+}
+
+//2 https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+    return dna.replace(/T/g, 'U');
+}
+
+//3 https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+var min = function (list) {
+    list.sort((a, b) => a - b);
+    return list[0];
+}
+
+var max = function (list) {
+    list.sort((a, b) => b - a);
+    return list[0];
+}
+
+// Solution by (Name)
