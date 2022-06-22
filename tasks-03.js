09c7a88c103fafcc91ffc5e079d30d4e29953fdc
VladGraundq <106146104+VladGraund@users.noreply.github.com>
2022-06-22 22:17:57 +0300

    Create tasks-03.js

---
+++ tasks-03.js
@@ -0,0 +1,13 @@
+//solution by Vlad
+//1 - Find the smallest integer in the array
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+//2 Geometry Basics: Circle Circumference in 2D
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  return Math.PI*circle.radius*2;
+}
