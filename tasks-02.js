3739bafa7f1315df8aaf14c6ddf9bd680521d868
Iryna Shtynda <ira.shtynda@gmail.com>
2022-06-22 10:02:06 +0300

    added solutins for task 2

--- tasks-02.js
+++ tasks-02.js
@@ -28,8 +28,30 @@ var max = function (list) {
 //4 https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
 function min(arr, toReturn) {
-    let value = Math.min.apply(null,arr);
+    let value = Math.min.apply(null, arr);
     return toReturn === 'value' ? val : arr.indexOf(val);
 }
 
-// Solution by (Name)
+// Solution by Iryna
+//1. https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
+    return string.split(' ');
+}
+
+//2. https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+function DNAtoRNA(dna) {
+    return dna.split('T').join('U');
+}
+
+//3. https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript 
+var min = function (list) {
+    return Math.min(...list);
+};
+var max = function (list) {
+    return Math.max(...list);
+};
+
+//4. https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+function min(arr, toReturn) {
+    return toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
+}
