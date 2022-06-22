ba4255f3986273954e7c86d1f4ad777c19acdfb8
Iryna Shtynda <105874801+IraShtynda@users.noreply.github.com>
2022-06-22 18:42:58 +0300

    corrected 6 exercise

--- tasks-02.js
+++ tasks-02.js
@@ -1,13 +1,14 @@
 // Звичайні задачі:
-
 //1 https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 function stringToArray(string) {
   return string.split(" ");
 }
+
 //2 https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
   return dna.replace(/T/g, "U");
 }
+
 //3 https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 var min = function (list) {
   return Math.min(...list);
@@ -15,6 +16,7 @@ var min = function (list) {
 var max = function (list) {
   return Math.max(...list);
 };
+
 //4 https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 function min(arr, toReturn) {
   const minValue = Math.min(...arr);
@@ -31,10 +33,12 @@ function warnTheSheep(queue) {
     queue.length - queue.indexOf("wolf") - 1
   }! You are about to be eaten by a wolf!`;
 }
+
 //2 https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
 function maps(x) {
   return x.map((num) => num * 2);
 }
+
 //3 https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
 function firstNonConsecutive(arr) {
   for (let i = 1; i < arr.length; i++) {
@@ -44,45 +48,55 @@ function firstNonConsecutive(arr) {
   }
   return null;
 }
+
 //Додаткові задачки
 //1. https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
 function doubleInteger(i) {
   return 2 * i;
 }
+
 //2. https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
 function century(year) {
   return Math.ceil(year / 100);
 }
+
 //3. https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
 function multiply(numberOne, numberTwo) {
   return numberOne * numberTwo;
 }
+
 //4. https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
 class Kata {
   static getVolumeOfCuboid(length, width, height) {
     return length * width * height;
   }
 }
+
 //5. https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
   return -Math.abs(num);
 }
+
 //6. https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
 function cockroachSpeed(s) {
-  return parseInt((s * 100000) / 3600);
+  return Math.floor(s * 100000 / 3600);
 }
+
 //7. https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
 function otherAngle(a, b) {
   return 180 - a - b;
 }
+
 //8. https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
+
 //9. https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
 function nthEven(n) {
   return 2 * (n - 1);
 }
+
 //10. https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
 function getRealFloor(n) {
   if (n < 1) {
@@ -93,10 +107,12 @@ function getRealFloor(n) {
   }
   return n - 2;
 }
+
 //11. https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
 function past(h, m, s) {
   return 1000 * (s + 60 * (m + 60 * h));
 }
+
 //12. https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
 function isDivisible(n, x, y) {
   return n % y == 0 && n % x == 0 ? true : false;
