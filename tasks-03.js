d5163387c4a4ae337b1e6e8236c6b686df9d3d1e
juliatomi7 <juliiatomashevska@gmail.com>
2022-06-25 21:21:07 +0000

    Added my solution for step 3

--- tasks-03.js
+++ tasks-03.js
@@ -103,4 +103,71 @@ function buildFun(n) {
   return res;
 }
 
+//solution by Yuliia
+//1. https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.sort(function(a,b){
+      return a - b;
+    })[0]
+  }
+}
+
+//2. https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  return Math.PI * circle.radius * 2;
+}
+
+//3. https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  var fiveLetter = [];
+  for (var key in obj) {
+    if (key.length === 5) {
+      fiveLetter.push(key);
+    }
+    if (obj[key].length === 5) {
+      fiveLetter.push(obj[key]);
+    }
+  }
+  return fiveLetter;
+}
+
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+function buildFun(n){
+	var res = []
+	for (let i = 0; i< n; i++){
+		res.push(function(){
+      return i;
+		})
+	}
+	return res
+}
+
+// Поглибленні:
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return super.introduce()+"  Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster(){
+    return "Hello " + this.master;
+  }
+}
 
