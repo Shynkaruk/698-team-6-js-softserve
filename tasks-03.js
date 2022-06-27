cdc63c4d37cd6990f7299a6c50b522e01bc80424
yura4ch <yura4ch@gmail.com>
2022-06-27 14:50:49 +0300

    Added 5 task by Yurii Shyndor

--- tasks-03.js
+++ tasks-03.js
@@ -224,3 +224,30 @@ function giveMeFive(obj){
   return fiveArr
 }
 
+// task 4
+//...
+
+//task 5
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+}
+  introduce() {
+    return `{$super.introduce()} "Meow meow!"`;
+  }
+}
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
